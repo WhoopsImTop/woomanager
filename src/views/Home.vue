74ca54355c601a15bd088827cc9881d47bbb104b
elias <eliasenglen2@gmail.com>
2021-10-27 22:35:36 +0200

    add requestor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,18 @@
 <template>
-<div></div>
+  <div>
+    <v-btn color="primary" @click="getData()">GET REQUEST</v-btn>
+  </div>
 </template>
 
 <script>
-
+import requestor from '../helpers/requestor'
 export default {
   name: 'Home',
+  methods: {
+    async getData() {
+      let data = await requestor("GET", "products", "per_page=100")
+      console.log(data)
+    }
+  }
 }
 </script>
